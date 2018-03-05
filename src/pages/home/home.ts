ad86eca43ef515e9442663b6f678a7294a283ac6
shrimank <shrimankumbar@gmail.com>
2018-03-05 11:16:22 +0530

    Initial Commit.

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,8 @@
-import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { Component, NgZone,ViewChild } from '@angular/core';
+import { TextToSpeech } from '@ionic-native/text-to-speech';
+import { Content } from 'ionic-angular';
+
+declare var window;
 
 @Component({
   selector: 'page-home',
@@ -7,8 +10,53 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  messages: any[] = [];
+  text: string = '';
+  @ViewChild(Content) content;
 
+  constructor(public ngZone: NgZone, private tts: TextToSpeech) {
+    this.messages.push({
+      text: 'Hi,How can I help you?',
+      sender: 'api'
+    });
+  }
+
+  sendText() {
+    let message = this.text;
+    this.messages.push({
+      text: message,
+      sender: 'me'
+    });
+    this.content.scrollToBottom(200);
+    this.text = '';
+    window['ApiAIPlugin'].requestText({
+      query: message
+    }, (response) => {
+
+      this.ngZone.run(() => {
+        this.messages.push({
+          text: response.result.fulfillment.speech,
+          sender: 'api'
+        });
+        this.content.scrollToBottom(200);
+      });
+
+    }, (error) => {
+      alert("Error:" + JSON.stringify(error));
+    });
+  }
+
+  sendVoice() {
+
+    window['ApiAIPlugin'].requestVoice({},
+      (response) => {
+          this.tts.speak({ 
+            text: response.result.fulfillment.speech, 
+            locale: 'en-IN', 
+            rate: 1 });
+      }, (error) => {
+        alert("Error:Voice" + JSON.stringify(error));
+      });
   }
 
 }
